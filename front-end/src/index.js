debe79d045d8e6cda50f1efe67bc1a3dbffa571c
Jan Meininghaus <git@janmeininghaus.com>
2019-06-09 23:55:26 +0200

    added node graph editor

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import init from "./editor";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "./styles.css";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <div style={{ textAlign: "left", width: "100vw", height: "70vh" }}>
+        <div ref={el => init(el)} />
+      </div>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
