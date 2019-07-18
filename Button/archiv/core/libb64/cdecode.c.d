476ff0bb09921aecbaec4211e874c0377bfa0c85
Jan Meininghaus <git@janmeininghaus.com>
2019-07-18 18:55:31 +0200

    removed ino build files + updated gitignore

--- Button/archiv/core/libb64/cdecode.c.d
+++
@@ -1,7 +0,0 @@
-/Applications/XAMPP/xamppfiles/htdocs/IoT-Backend/Button/archiv/core/libb64/cdecode.c.o: \
- /Users/janmeininghaus/Library/Arduino15/packages/esp8266/hardware/esp8266/2.5.0/cores/esp8266/libb64/cdecode.c \
- /Users/janmeininghaus/Library/Arduino15/packages/esp8266/hardware/esp8266/2.5.0/cores/esp8266/libb64/cdecode.h \
- /Users/janmeininghaus/Library/Arduino15/packages/esp8266/hardware/esp8266/2.5.0/tools/sdk/libc/xtensa-lx106-elf/include/stdint.h \
- /Users/janmeininghaus/Library/Arduino15/packages/esp8266/hardware/esp8266/2.5.0/tools/sdk/libc/xtensa-lx106-elf/include/machine/_default_types.h \
- /Users/janmeininghaus/Library/Arduino15/packages/esp8266/hardware/esp8266/2.5.0/tools/sdk/libc/xtensa-lx106-elf/include/sys/features.h \
- /Users/janmeininghaus/Library/Arduino15/packages/esp8266/hardware/esp8266/2.5.0/tools/sdk/libc/xtensa-lx106-elf/include/sys/_intsup.h
